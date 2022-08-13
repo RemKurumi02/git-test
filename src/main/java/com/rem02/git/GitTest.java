6bc00c09960286919d219b726d49988d5d53f6d9
Rem02 <957763931@qq.com>
2022-08-13 11:02:53 +0800

    first commit

---
+++ src/main/java/com/rem02/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.rem02.git;
+
+/**
+ * @author Rem02       Email:957763931@qq.com
+ * @Description
+ * @create 2022-08-13 10:43     Tool:IDEA 2020.1
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
