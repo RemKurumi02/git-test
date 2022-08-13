368833e8c823295469f8857162fc65b088e894e6
Rem02 <957763931@qq.com>
2022-08-13 12:17:38 +0800

    push commit

--- src/main/java/com/rem02/git/GitTest.java
+++ src/main/java/com/rem02/git/GitTest.java
@@ -15,6 +15,7 @@ public class GitTest {
         System.out.println("hot-fix test2");
         System.out.println("master test");
         System.out.println("master test2");
+        System.out.println("push test");
 
     }
 }
